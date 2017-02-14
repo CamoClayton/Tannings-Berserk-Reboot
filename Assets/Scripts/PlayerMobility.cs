ed6f5e8de8ed99b85651739c71945a6367071200
Private\Don <1006749@student.sae.edu.au>
2017-02-15 09:20:27 +1000

    Updated First repo. I hope this fucking works!!

---
+++ Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMobility : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+        //gets player to look in direction of mouse and speed rotation
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        //sets rotation on z axis only
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+       
+        
+
+    }
+}
+
+	
