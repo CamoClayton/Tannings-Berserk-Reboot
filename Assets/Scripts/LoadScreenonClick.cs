5474ff8bd9be08600db6fb39ba3b7a79933370de
Private\Don <1006749@student.sae.edu.au>
2017-02-19 10:28:49 +1000

    Addition of Levels and changes.

---
+++ Assets/Scripts/LoadScreenonClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScreenonClick : MonoBehaviour
+{
+
+    public class LoadSceneOnClick : MonoBehaviour
+    {
+        //Load scene according to order in scene index
+        public void LoadByIndex(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rb : MonoBehaviour
+{
+    public float speed;
+   
+
+   void start()
+    {
+       
+    }
+
+		void FixedUpdate ()
+    {
+        //gets player to look in direction of mouse and at what speed it rotates to look.
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        //setsrotation to Z axis only.
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+       
+	}
+	
+}
